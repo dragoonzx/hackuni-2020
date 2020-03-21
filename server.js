9ec0fe7f0857c659c0a8d0ce24c720e5591cb2c6
Max <dragoonz@yandex.ru>
2020-03-21 20:47:56 +0300

    server

--- server.js
+++ server.js
@@ -1,48 +1,8 @@
-<template>
-  <div class="hello">
-    <div class="left-menu">
-      <div class="info">
-        <h2>Tracking your cargo safety</h2>
-        <p>Bla bla bla</p>
-      </div>
-      <div class="cargo-list">
-        <button @click.prevent="apiRequest">Click</button>
-      </div>
-    </div>
-    <div class="main-menu">
-      <div ref="map" class="interactive-map" style="width: calc(100% - 30rem); height: 100vh">
-      </div>
-    </div>
-  </div>
-</template>
+const fetch = require('node-fetch')
 
-<script>
-export default {
-  name: 'hello',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js PWA',
-      map: {},
-      platform: {},
-      appId: 'dWuK9yGmEm8ukYeHW5f4',
-      appCode: 'hrieRalCaIPMe1vXiZTw6_Amyfi2VVomjBvOLmz1lOs'
-    };
-  },
-  methods: {
-    setStyle: function(map) {
-        // get the vector provider from the base layer
-        var provider = map.getBaseLayer().getProvider();
-        // Create the style object from the YAML configuration.
-        // First argument is the style path and the second is the base URL to use for
-        // resolving relative URLs in the style like textures, fonts.
-        // all referenced resources relative to the base path https://js.api.here.com/v3/3.1/styles/omv.
-        let style = new H.map.Style('https://heremaps.github.io/maps-api-for-javascript-examples/change-style-at-load/data/dark.yaml',
-          'https://js.api.here.com/v3/3.1/styles/omv/');
-        // set the style on the existing layer
-        provider.setStyle(style);
-      },
-    apiRequest: async function() {
-      const response = await fetch('https://dev.rightech.io/api/v1/objects/http-hu2020_09-9i4lan/packets', {
+
+const getData = async () => {
+    const response = await fetch('https://dev.rightech.io/api/v1/objects/http-hu2020_09-9i4lan/packets', {
         method: 'POST',
         headers: {
           'content-Type': 'application/json',
@@ -56,84 +16,6 @@ export default {
       console.log(await response)
       let data = await response.json()
       console.log(data)
-    }
-  },
-  created() {
-    this.platform = new H.service.Platform({
-      apikey: this.appCode
-    });
-  },
-  mounted() {
-    let maptypes = this.platform.createDefaultLayers();
-
-    this.map = new H.Map(
-        this.$refs.map,
-        maptypes.vector.normal.map,
-        {
-            zoom: 10,
-            center: { lat: 59.9342802, lng: 30.3350986 }
-        }
-    );
-
-    window.addEventListener('resize', () => this.map.getViewPort().resize());
-
-    let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
-
-    // Create the default UI components
-    let ui = H.ui.UI.createDefault(this.map, maptypes);
-    
-    this.setStyle(this.map);
-  }
-};
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-h1, h2 {
-  font-weight: normal;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #35495E;
-}
-
-.left-menu {
-  position: fixed;
-  top: 56px;
-  left: 0;
-  bottom: 0;
-  z-index: 999999;
-  border-top: 0.25rem solid #00AFAA;
-  width: 30rem;
-  box-shadow: 0 10px 15px -3px rgba(0,0,0,0.1), 0 4px 6px -2px rgba(0,0,0,0.05);
-  overflow: scroll;
-  background-color: #fff;
-  padding-bottom: 5rem;
-}
-.left-menu .info {
-  padding-left: 1.25rem;
-  padding-right: 1.25rem;
-  padding-top: 1.25rem;
-}
-.main-menu {
-    position: fixed;
-    transition: opacity 500ms ease 0s;
-    opacity: 1;
-    cursor: default;
-    outline: none;
-    left: 30rem;
-    top: 56px;
-    width: 100%;
-    height: 100%;
-}
-</style>
+getData()
